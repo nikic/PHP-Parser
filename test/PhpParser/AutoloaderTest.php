5f621c5adcef9bd01a6b3d0579bf5b0a9007d4f8
Nikita Popov <nikita.ppv@googlemail.com>
2015-05-02 22:19:25 +0200

    Drop support for legacy aliases from v0.9

--- test/PhpParser/AutoloaderTest.php
+++ test/PhpParser/AutoloaderTest.php
@@ -8,16 +8,6 @@ use PhpParser\Node\Scalar;
 /* The autoloader is already active at this point, so we only check effects here. */
 
 class AutoloaderTest extends \PHPUnit_Framework_TestCase {
-    public function testLegacyNames() {
-        $lexer = new \PHPParser_Lexer;
-        $parser = new \PHPParser_Parser($lexer);
-        $prettyPrinter = new \PHPParser_PrettyPrinter_Default;
-
-        $this->assertInstanceof('PhpParser\Lexer', $lexer);
-        $this->assertInstanceof('PhpParser\Parser', $parser);
-        $this->assertInstanceof('PhpParser\PrettyPrinter\Standard', $prettyPrinter);
-    }
-
     public function testPhp7ReservedNames() {
         if (version_compare(PHP_VERSION, '7.0-dev', '>=')) {
             $this->markTestSkipped('Cannot create aliases to reserved names on PHP 7');
@@ -33,7 +23,7 @@ class AutoloaderTest extends \PHPUnit_Framework_TestCase {
 
     public function testClassExists() {
         $this->assertTrue(class_exists('PhpParser\NodeVisitorAbstract'));
-        $this->assertTrue(class_exists('PHPParser_NodeVisitor_NameResolver'));
+        $this->assertFalse(class_exists('PHPParser_NodeVisitor_NameResolver'));
 
         $this->assertFalse(class_exists('PhpParser\FooBar'));
         $this->assertFalse(class_exists('PHPParser_FooBar'));
