a46b3099758ac75fbbaa9e33a3716267ffb0d48d
Nikita Popov <nikita.ppv@googlemail.com>
2016-10-16 22:13:09 +0200

    Move constants into NodeTraverser class
    Not that the interface makes much sense anyway, but these are
    implementation details of a specific traverser.

--- lib/PhpParser/NodeTraverserInterface.php
+++ lib/PhpParser/NodeTraverserInterface.php
@@ -5,24 +5,6 @@ namespace PhpParser;
 interface NodeTraverserInterface
 {
     /**
-     * If NodeVisitor::enterNode() returns DONT_TRAVERSE_CHILDREN, child nodes
-     * of the current node will not be traversed for any visitors.
-     *
-     * For subsequent visitors enterNode() will still be called on the current
-     * node and leaveNode() will also be invoked for the current node.
-     */
-    const DONT_TRAVERSE_CHILDREN = 1;
-
-    /**
-     * If NodeVisitor::leaveNode() returns REMOVE_NODE for a node that occurs
-     * in an array, it will be removed from the array.
-     *
-     * For subsequent visitors leaveNode() will still be invoked for the
-     * removed node.
-     */
-    const REMOVE_NODE = false;
-
-    /**
      * Adds a visitor.
      *
      * @param NodeVisitor $visitor Visitor to add
