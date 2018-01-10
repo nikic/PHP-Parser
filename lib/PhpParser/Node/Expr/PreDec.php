e6e52abae7b2f6dc455970fc6057f9a1c472af0d
Gabriel Caruso <carusogabriel34@gmail.com>
2018-01-10 14:28:00 -0200

    [CS] Trim whitespaces

--- lib/PhpParser/Node/Expr/PreDec.php
+++ lib/PhpParser/Node/Expr/PreDec.php
@@ -23,7 +23,7 @@ class PreDec extends Expr
     public function getSubNodeNames() : array {
         return ['var'];
     }
-    
+
     public function getType() : string {
         return 'Expr_PreDec';
     }
