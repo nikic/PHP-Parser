76965dc0a6bc6d76e761d3f91493d0fe57ebcfc9
Tomas Votruba <tomas.vot@gmail.com>
2019-02-01 23:01:34 +0100

    add Coalesce Assign to assignsOps

--- lib/PhpParser/PrettyPrinterAbstract.php
+++ lib/PhpParser/PrettyPrinterAbstract.php
@@ -1157,7 +1157,7 @@ abstract class PrettyPrinterAbstract
             Expr\Assign::class, Expr\AssignRef::class, AssignOp\Plus::class, AssignOp\Minus::class,
             AssignOp\Mul::class, AssignOp\Div::class, AssignOp\Concat::class, AssignOp\Mod::class,
             AssignOp\BitwiseAnd::class, AssignOp\BitwiseOr::class, AssignOp\BitwiseXor::class,
-            AssignOp\ShiftLeft::class, AssignOp\ShiftRight::class, AssignOp\Pow::class,
+            AssignOp\ShiftLeft::class, AssignOp\ShiftRight::class, AssignOp\Pow::class, AssignOp\Coalesce::class
         ];
         foreach ($assignOps as $assignOp) {
             $this->fixupMap[$assignOp] = [
