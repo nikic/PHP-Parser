ac9526d0ef5257d307ce82b58844994e19a8c3eb
Remi Collet <remi@remirepo.net>
2020-06-18 15:07:15 +0200

    generate stub from command + theseer/autoload template

--- build/makephar.sh
+++ build/makephar.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
+set -e
 cd $(dirname $0)/..
+set -x
 
+: Get version from changelog
 VER=$(head -n1 CHANGELOG.md  | cut -d' ' -f2)
-echo "Get version from changelog: $VER"
 
+: Create stub
+php -r '
+	$cmd = file_get_contents("bin/php-parse");
+	$pc = strpos($cmd, "// END AUTOLOADER");
+	$aut = file_get_contents("vendor/theseer/autoload/src/templates/ci/phar.php.tpl");
+	$pa = strpos($aut, "__HALT_COMPILER");
+	if ($cmd && $pc && $aut && $pa) {
+		file_put_contents("build/php-parse.tpl", substr($aut, 0, $pa) .	substr($cmd, $pc) .	substr($aut, $pa));
+	} else {
+		exit (1);
+	}
+'
+: Create phar
 php -d phar.readonly=0 \
 vendor/bin/phpab \
   --phar \
