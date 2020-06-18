4bb33d9b6143f1f09edad646938f68af7774d142
Remi Collet <remi@remirepo.net>
2020-06-18 11:32:44 +0200

    Generate a phar distribution

---
+++ build/makephar.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd $(dirname $0)/..
+
+VER=$(head -n1 CHANGELOG.md  | cut -d' ' -f2)
+echo "Get version from changelog: $VER"
+
+vendor/bin/phpab \
+  --phar \
+  --all \
+  --bzip2 \
+  --output build/php-parse-$VER.phar \
+  --template build/php-parse.tpl \
+  lib
+
