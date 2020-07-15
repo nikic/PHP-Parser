6cf39fe7384b46765da15c5c9f7c70a1a1769f29
Nikita Popov <nikita.ppv@gmail.com>
2020-07-15 21:31:00 +0200

    Switch run-php-src.sh to test against master
    This has more failures, but it's more honest about which PHP 8
    features we don't support yet ...

--- test_old/run-php-src.sh
+++ test_old/run-php-src.sh
@@ -1,4 +1,5 @@
-wget -q https://github.com/php/php-src/archive/PHP-7.4.tar.gz
+VERSION="master"
+wget -q https://github.com/php/php-src/archive/$VERSION.tar.gz
 mkdir -p ./data/php-src
-tar -xzf ./PHP-7.4.tar.gz -C ./data/php-src --strip-components=1
+tar -xzf ./$VERSION.tar.gz -C ./data/php-src --strip-components=1
 php -n test_old/run.php --verbose --no-progress PHP7 ./data/php-src
