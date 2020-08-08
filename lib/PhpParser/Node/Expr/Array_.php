55005e65068c548b3e367101c00111cf36fcd3b5
Alex <alex.deleyn@gmail.com>
2020-08-08 09:51:06 +0200

    Update Array_ annotations, items are nullable
    When used to destructure, items are optional. E.g. `[$a, , $b] = [1, 2, 3];`.

--- lib/PhpParser/Node/Expr/Array_.php
+++ lib/PhpParser/Node/Expr/Array_.php
@@ -10,13 +10,13 @@ class Array_ extends Expr
     const KIND_LONG = 1;  // array() syntax
     const KIND_SHORT = 2; // [] syntax
 
-    /** @var ArrayItem[] Items */
+    /** @var (ArrayItem|null)[] Items */
     public $items;
 
     /**
      * Constructs an array node.
      *
-     * @param ArrayItem[] $items      Items of the array
+     * @param (ArrayItem|null)[] $items      Items of the array
      * @param array       $attributes Additional attributes
      */
     public function __construct(array $items = [], array $attributes = []) {
