12e9e2ce4a1615e1b5081a3fc76c3c2e81846bf1
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2020-08-09 12:08:45 +0100

    Pin to specific commits

--- test_old/run-php-src.sh
+++ test_old/run-php-src.sh
@@ -1,7 +1,7 @@
 if [[ $1 == '7' ]]; then
-	VERSION='PHP-7.4'
+	VERSION='fa9bd812fcab6dfd6d9b506cb3cb04dfa75d239d'
 else
-	VERSION='master'
+	VERSION='05478e985eb50c473054b4f1bf174f48ead78784'
 fi
 wget -q https://github.com/php/php-src/archive/$VERSION.tar.gz
 mkdir -p ./data/php-src
