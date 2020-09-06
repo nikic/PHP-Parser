e3872b89063c715b2921420e6d447f4473c739af
Nikita Popov <nikita.ppv@gmail.com>
2020-09-06 15:00:28 +0200

    Improve compatibility with other libs defining compat tokens

--- lib/PhpParser/Lexer.php
+++ lib/PhpParser/Lexer.php
@@ -405,33 +405,57 @@ class Lexer
     }
 
     private function defineCompatibilityTokens() {
-        // PHP 7.4
-        if (!defined('T_BAD_CHARACTER')) {
-            \define('T_BAD_CHARACTER', -1);
-        }
-        if (!defined('T_FN')) {
-            \define('T_FN', -2);
-        }
-        if (!defined('T_COALESCE_EQUAL')) {
-            \define('T_COALESCE_EQUAL', -3);
+        static $compatTokensDefined = false;
+        if ($compatTokensDefined) {
+            return;
         }
 
-        // PHP 8.0
-        if (!defined('T_NAME_QUALIFIED')) {
-            \define('T_NAME_QUALIFIED', -4);
+        $compatTokens = [
+            // PHP 7.4
+            'T_BAD_CHARACTER',
+            'T_FN',
+            'T_COALESCE_EQUAL',
+            // PHP 8.0
+            'T_NAME_QUALIFIED',
+            'T_NAME_FULLY_QUALIFIED',
+            'T_NAME_RELATIVE',
+            'T_MATCH',
+            'T_NULLSAFE_OBJECT_OPERATOR',
+        ];
+
+        // PHP-Parser might be used together with another library that also emulates some or all
+        // of these tokens. Perform a sanity-check that all already defined tokens have been
+        // assigned a unique ID.
+        $usedTokenIds = [];
+        foreach ($compatTokens as $token) {
+            if (\defined($token)) {
+                $tokenId = \constant($token);
+                $clashingToken = $usedTokenIds[$tokenId] ?? null;
+                if ($clashingToken !== null) {
+                    throw new \Error(sprintf(
+                        'Token %s has same ID as token %s, ' .
+                        'you may be using a library with broken token emulation',
+                        $token, $clashingToken
+                    ));
+                }
+                $usedTokenIds[$token] = $tokenId;
+            }
         }
-        if (!defined('T_NAME_FULLY_QUALIFIED')) {
-            \define('T_NAME_FULLY_QUALIFIED', -5);
+
+        // Now define any tokens that have not yet been emulated. Try to assign IDs from -1
+        // downwards, but skip any IDs that may already be in use.
+        $newTokenId = -1;
+        foreach ($compatTokens as $token) {
+            if (!\defined($token)) {
+                while (isset($usedTokenIds[$newTokenId])) {
+                    $newTokenId--;
+                }
+                \define($token, $newTokenId);
+                $newTokenId--;
+            }
         }
-        if (!defined('T_NAME_RELATIVE')) {
-            \define('T_NAME_RELATIVE', -6);
-        }
-        if (!defined('T_MATCH')) {
-            \define('T_MATCH', -7);
-        }
-        if (!defined('T_NULLSAFE_OBJECT_OPERATOR')) {
-            \define('T_NULLSAFE_OBJECT_OPERATOR', -8);
-        }
+
+        $compatTokensDefined = true;
     }
 
     /**
