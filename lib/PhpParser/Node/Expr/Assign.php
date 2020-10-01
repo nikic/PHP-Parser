a8336be5d29f08633dc24054975b1fac7c06b384
David Rodrigues <david.proweb@gmail.com>
2020-10-01 18:41:49 -0300

    Assign: extends AssignOp;

--- lib/PhpParser/Node/Expr/Assign.php
+++ lib/PhpParser/Node/Expr/Assign.php
@@ -2,32 +2,8 @@
 
 namespace PhpParser\Node\Expr;
 
-use PhpParser\Node\Expr;
-
-class Assign extends Expr
+class Assign extends AssignOp
 {
-    /** @var Expr Variable */
-    public $var;
-    /** @var Expr Expression */
-    public $expr;
-
-    /**
-     * Constructs an assignment node.
-     *
-     * @param Expr  $var        Variable
-     * @param Expr  $expr       Expression
-     * @param array $attributes Additional attributes
-     */
-    public function __construct(Expr $var, Expr $expr, array $attributes = []) {
-        $this->attributes = $attributes;
-        $this->var = $var;
-        $this->expr = $expr;
-    }
-
-    public function getSubNodeNames() : array {
-        return ['var', 'expr'];
-    }
-    
     public function getType() : string {
         return 'Expr_Assign';
     }
