0f64504317ea99029915203924160cbd24400bdd
Nikita Popov <nikita.ppv@gmail.com>
2020-12-08 23:05:19 +0100

    Drop -n flag from test_old
    In the hope of fixing the failure on GH actions.

--- test_old/run-php-src.sh
+++ test_old/run-php-src.sh
@@ -2,4 +2,4 @@ VERSION=$1
 wget -q https://github.com/php/php-src/archive/php-$VERSION.tar.gz
 mkdir -p ./data/php-src
 tar -xzf ./php-$VERSION.tar.gz -C ./data/php-src --strip-components=1
-php -n test_old/run.php --verbose --no-progress --php-version=$VERSION PHP ./data/php-src
+php test_old/run.php --verbose --no-progress --php-version=$VERSION PHP ./data/php-src
