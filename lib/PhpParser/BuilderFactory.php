a33e8fac221d7d044e0c1436b3d72cd6a8d831ae
WhizSid <whizsid@aol.com>
2021-03-21 17:36:52 +0530

    Adding class constant to builder factory

--- lib/PhpParser/BuilderFactory.php
+++ lib/PhpParser/BuilderFactory.php
@@ -162,6 +162,18 @@ class BuilderFactory
     }
 
     /**
+     * Creates a class constant builder.
+     *
+     * @param string|Identifier                          $name  - Name
+     * @param Node\Expr|bool|null|int|float|string|array $value — Value
+     *
+     * @return Builder\Use_ The created use const builder
+     */
+    public function classConst($name, $value) : Builder\ClassConst {
+        return new Builder\ClassConst($name, $value);
+    }
+
+    /**
      * Creates node a for a literal value.
      *
      * @param Expr|bool|null|int|float|string|array $value $value
