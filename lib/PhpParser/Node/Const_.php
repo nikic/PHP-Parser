f4b835f7d858d90b58c6d965a7d0f87d2ba57c43
Ondrej Mirtes <ondrej@mirtes.cz>
2021-12-06 21:21:44 +0100

    Fix PHPDoc type of namespacedName properties

--- lib/PhpParser/Node/Const_.php
+++ lib/PhpParser/Node/Const_.php
@@ -11,7 +11,7 @@ class Const_ extends NodeAbstract
     /** @var Expr Value */
     public $value;
 
-    /** @var Name Namespaced name (if using NameResolver) */
+    /** @var Name|null Namespaced name (if using NameResolver) */
     public $namespacedName;
 
     /**
@@ -30,7 +30,7 @@ class Const_ extends NodeAbstract
     public function getSubNodeNames() : array {
         return ['name', 'value'];
     }
-    
+
     public function getType() : string {
         return 'Const';
     }
