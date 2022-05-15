231679112e2af72244d167212328e59801f64aea
Tomas Votruba <tomas.vot@gmail.com>
2022-05-16 00:24:40 +0200

    add StmtsIterableTest

---
+++ test/PhpParser/Node/StmtsIterableTest.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace PhpParser\Node;
+
+use PhpParser\Node\Stmt\Foreach_;
+use PhpParser\Node\Stmt\Function_;
+use PhpParser\NodeFinder;
+use PhpParser\ParserFactory;
+
+class StmtsIterableTest extends \PHPUnit\Framework\TestCase
+{
+    public function test()
+    {
+        $parser = (new ParserFactory())->create(ParserFactory::PREFER_PHP7);
+        $nodes = $parser->parse(<<<'CODE_SAMPLE'
+<?php
+
+function clearItemList($items)
+{
+    foreach ($items as $key => $value) {
+        $value = 100;
+        $value = 100;
+    }
+}
+CODE_SAMPLE
+        );
+
+        $nodeFinder = new NodeFinder();
+        $stmtsIterables = $nodeFinder->findInstanceOf($nodes, StmtsIterable::class);
+
+        $this->assertCount(2, $stmtsIterables);
+        $this->assertInstanceOf(Function_::class, $stmtsIterables[0]);
+        $this->assertInstanceOf(Foreach_::class, $stmtsIterables[1]);
+    }
+}
