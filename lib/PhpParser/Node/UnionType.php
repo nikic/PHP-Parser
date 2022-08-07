9b2a01aa0cd0f97cd2d10ec5e17a1bf8b3fe142a
George Peter Banyard <girgias@php.net>
2022-08-07 16:10:11 +0100

    Add support for DNF types (#862)

--- lib/PhpParser/Node/UnionType.php
+++ lib/PhpParser/Node/UnionType.php
@@ -10,7 +10,7 @@ class UnionType extends ComplexType
     /**
      * Constructs a union type.
      *
-     * @param (Identifier|Name)[] $types      Types
+     * @param (Identifier|Name|IntersectionType)[] $types      Types
      * @param array               $attributes Additional attributes
      */
     public function __construct(array $types, array $attributes = []) {
