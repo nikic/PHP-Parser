dc3b98e264cbd74426707d20029972bcd02db5d2
Anton <info@ensostudio.ru>
2022-08-16 00:21:10 +0300

    Access to related node in PhpParser\Comment\Doc
    The comment context useful to customize printing of comments by PrettyPrinter's

--- lib/PhpParser/Comment/Doc.php
+++ lib/PhpParser/Comment/Doc.php
@@ -2,6 +2,32 @@
 
 namespace PhpParser\Comment;
 
-class Doc extends \PhpParser\Comment
+use PhpParser\Comment;
+use PhpParser\Error;
+use PhpParser\Node;
+
+/**
+ * The doc comment.
+ */
+class Doc extends Comment
 {
+    /** @var null|Node The comment node */
+    protected $node;
+
+    /**
+     * @throws Error if the node not related to the comment
+     */
+    public function setNode(Node $node)
+    {
+        $comment = $node->getDocComment();
+        if ($comment != $this) {
+            throw new Error('The node not related to the comment.', $node->getAttributes());
+        }
+        $this->node = $node;
+    }
+
+    public function getNode() ?Node
+    {
+        return $this->node;
+    }
 }
