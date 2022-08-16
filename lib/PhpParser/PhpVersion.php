199d6f42cb398303db23bcb4b34de07116889230
Anton <info@ensostudio.ru>
2022-08-16 03:55:03 +0300

    Update PhpVersion.php

--- lib/PhpParser/PhpVersion.php
+++ lib/PhpParser/PhpVersion.php
@@ -35,7 +35,7 @@ class PhpVersion
     /**
      * Create a PhpVersion object from major and minor version components.
      */
-    public static function fromComponents(int $major, int $minor): self {
+    public static function fromComponents(int $major, int $minor) : self {
         return new self($major * 10000 + $minor * 100);
     }
 
@@ -43,21 +43,21 @@ class PhpVersion
      * Get the newest PHP version supported by this library. Support for this version may be partial,
      * if it is still under development.
      */
-    public static function getNewestSupported(): self {
+    public static function getNewestSupported() : self {
         return self::fromComponents(8, 2);
     }
 
     /**
      * Get the host PHP version, that is the PHP version we're currently running on.
      */
-    public static function getHostVersion(): self {
+    public static function getHostVersion() : self {
         return self::fromComponents(\PHP_MAJOR_VERSION, \PHP_MINOR_VERSION);
     }
 
     /**
      * Parse the version from a string like "8.1".
      */
-    public static function fromString(string $version): self {
+    public static function fromString(string $version) : self {
         if (!preg_match('/^(\d+)\.(\d+)/', $version, $matches)) {
             throw new \LogicException("Invalid PHP version \"$version\"");
         }
@@ -67,35 +67,35 @@ class PhpVersion
     /**
      * Check whether two versions are the same.
      */
-    public function equals(PhpVersion $other): bool {
+    public function equals(PhpVersion $other) : bool {
         return $this->id === $other->id;
     }
 
     /**
      * Check whether this version is greater than or equal to the argument.
      */
-    public function newerOrEqual(PhpVersion $other): bool {
+    public function newerOrEqual(PhpVersion $other) : bool {
         return $this->id >= $other->id;
     }
 
     /**
      * Check whether this version is older than the argument.
      */
-    public function older(PhpVersion $other): bool {
+    public function older(PhpVersion $other) : bool {
         return $this->id < $other->id;
     }
 
     /**
      * Check whether this is the host PHP version.
      */
-    public function isHostVersion(): bool {
+    public function isHostVersion() : bool {
         return $this->equals(self::getHostVersion());
     }
 
     /**
      * Check whether this PHP version supports the given builtin type. Type name must be lowercase.
      */
-    public function supportsBuiltinType(string $type): bool {
+    public function supportsBuiltinType(string $type) : bool {
         $minVersion = self::BUILTIN_TYPE_VERSIONS[$type] ?? null;
         return $minVersion !== null && $this->id >= $minVersion;
     }
@@ -103,42 +103,42 @@ class PhpVersion
     /**
      * Whether this version supports [] array literals.
      */
-    public function supportsShortArraySyntax(): bool {
+    public function supportsShortArraySyntax() : bool {
         return $this->id >= 50400;
     }
 
     /**
      * Whether this version supports [] list literals (symmetric array destructuring).
      */
-    public function supportsShortListSyntax(): bool {
+    public function supportsShortListSyntax() : bool {
         return $this->id >= 70100;
     }
 
     /**
      * Whether this version supports flexible heredoc/nowdoc.
      */
-    public function supportsFlexibleHeredoc(): bool {
+    public function supportsFlexibleHeredoc() : bool {
         return $this->id >= 70300;
     }
 
     /**
      * Whether this version supports trailing commas in parameter lists.
      */
-    public function supportsTrailingCommaInParamList(): bool {
+    public function supportsTrailingCommaInParamList() : bool {
         return $this->id >= 80000;
     }
 
     /**
      * Whether this version allows "$var =& new Obj".
      */
-    public function allowsAssignNewByReference(): bool {
+    public function allowsAssignNewByReference() : bool {
         return $this->id < 70000;
     }
 
     /**
      * Whether this version allows invalid octals like "08".
      */
-    public function allowsInvalidOctals(): bool {
+    public function allowsInvalidOctals() : bool {
         return $this->id < 70000;
     }
 }
