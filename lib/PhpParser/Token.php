5b691163ae360de7fa63f9b2a5c3fc48db4cd75d
Anton <info@ensostudio.ru>
2022-08-30 14:39:45 +0300

    Update Token.php

--- lib/PhpParser/Token.php
+++ lib/PhpParser/Token.php
@@ -8,15 +8,12 @@ namespace PhpParser;
 class Token extends Internal\TokenPolyfill {
     /** Get (exclusive) zero-based end position of the token. */
     public function getEndPos(): int {
-        return $this->pos < 0 ? -1 : $this->pos + \strlen($this->text);
+        return $this->pos < 0 ? $this->pos : $this->pos + \strlen($this->text);
     }
 
     /** Get 1-based end line number of the token. */
     public function getEndLine(): int {
-        if ($this->line < 1) {
-            return -1;
-        }
-        if (!isset($this->text[1])) {
+        if ($this->line < 1 || !isset($this->text[1])) {
             return $this->line;
         }
         return $this->line + \substr_count($this->text, "\n");
