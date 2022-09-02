9dca6f1d3719b9c81b579f1d95ea2c0be9624f08
Nikita Popov <nikita.ppv@gmail.com>
2022-09-02 22:54:29 +0200

    Add test for StaticVar rename
    Also run these in separate processes, they're not really meaningful
    if the classes are already loaded.

--- test/PhpParser/CompatibilityTest.php
+++ test/PhpParser/CompatibilityTest.php
@@ -3,21 +3,28 @@
 namespace PhpParser;
 
 use PhpParser\Node\Expr;
+use PhpParser\Node\Stmt;
 
 class CompatibilityTest extends \PHPUnit\Framework\TestCase {
+    /** @runInSeparateProcess */
     public function testAliases1() {
         $var = new Expr\Variable('x');
         $node = new Node\ClosureUse($var);
         $this->assertTrue($node instanceof Expr\ClosureUse);
         $node = new Node\ArrayItem($var);
         $this->assertTrue($node instanceof Expr\ArrayItem);
+        $node = new Node\StaticVar($var);
+        $this->assertTrue($node instanceof Stmt\StaticVar);
     }
 
+    /** @runInSeparateProcess */
     public function testAliases2() {
         $var = new Expr\Variable('x');
         $node = new Node\Expr\ClosureUse($var);
         $this->assertTrue($node instanceof Node\ClosureUse);
         $node = new Node\Expr\ArrayItem($var);
         $this->assertTrue($node instanceof Node\ArrayItem);
+        $node = new Node\Stmt\StaticVar($var);
+        $this->assertTrue($node instanceof Node\StaticVar);
     }
 }
