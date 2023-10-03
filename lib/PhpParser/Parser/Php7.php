2c9b452eced400ead6e2ed65fbd37ea7c5072a18
xjaja <5057757+xjaja@users.noreply.github.com>
2023-10-04 04:01:48 +0800

    Update Php7.php

--- lib/PhpParser/Parser/Php7.php
+++ lib/PhpParser/Parser/Php7.php
@@ -2056,7 +2056,7 @@ class Php7 extends \PhpParser\ParserAbstract
                  $this->semValue = new Stmt\StaticVar($this->semStack[$stackPos-(3-1)], $this->semStack[$stackPos-(3-3)], $this->startAttributeStack[$stackPos-(3-1)] + $this->endAttributes);
             },
             340 => function ($stackPos) {
-                 if ($this->semStack[$stackPos-(2-2)] !== null) { $this->semStack[$stackPos-(2-1)][] = $this->semStack[$stackPos-(2-2)]; $this->semValue = $this->semStack[$stackPos-(2-1)]; }
+                 if ($this->semStack[$stackPos-(2-2)] !== null) { $this->semStack[$stackPos-(2-1)][] = $this->semStack[$stackPos-(2-2)]; $this->semValue = $this->semStack[$stackPos-(2-1)]; } else { $this->semValue = $this->semStack[$stackPos-(2-1)]; }
             },
             341 => function ($stackPos) {
                  $this->semValue = array();
