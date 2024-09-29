ad38d472470c4be95eeb6bea190f04a22c1758c0
Nikita Popov <nikita.ppv@gmail.com>
2024-09-29 15:34:59 +0200

    Avoid negative indendation in formatting-preserving printer
    Fixes #1015.
    
    (cherry-picked from 961f158f6da32565e7e3a33e23b8a38941068597)

--- lib/PhpParser/PrettyPrinterAbstract.php
+++ lib/PhpParser/PrettyPrinterAbstract.php
@@ -658,7 +658,7 @@ abstract class PrettyPrinterAbstract
                 $result .= $extraLeft;
 
                 $origIndentLevel = $this->indentLevel;
-                $this->setIndentLevel($this->origTokens->getIndentationBefore($subStartPos) + $indentAdjustment);
+                $this->setIndentLevel(max($this->origTokens->getIndentationBefore($subStartPos) + $indentAdjustment, 0));
 
                 // If it's the same node that was previously in this position, it certainly doesn't
                 // need fixup. It's important to check this here, because our fixup checks are more
@@ -761,7 +761,7 @@ abstract class PrettyPrinterAbstract
                 \assert($itemStartPos >= 0 && $itemEndPos >= 0 && $itemStartPos >= $pos);
 
                 $origIndentLevel = $this->indentLevel;
-                $lastElemIndentLevel = $this->origTokens->getIndentationBefore($itemStartPos) + $indentAdjustment;
+                $lastElemIndentLevel = max($this->origTokens->getIndentationBefore($itemStartPos) + $indentAdjustment, 0);
                 $this->setIndentLevel($lastElemIndentLevel);
 
                 $comments = $arrItem->getComments();
