8df0e527e649cba7a1ebf125d4a2a1471f3206e4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-04 12:26:04 -0600

    Update DeclareDeclare.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- lib/PhpParser/Node/Stmt/DeclareDeclare.php
+++ lib/PhpParser/Node/Stmt/DeclareDeclare.php
@@ -1,3 +1,11 @@
 <?php declare(strict_types=1);
 
+namespace PhpParser\Node\Stmt;
+
+use PhpParser\Node\DeclareItem;
+
 require __DIR__ . '/../DeclareItem.php';
+
+if (false) {
+    class DeclareDeclare extends DeclareItem {}
+}
