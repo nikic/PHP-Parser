69b68b856376afe7877826ea00230f4e44e81375
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-04 12:26:04 -0600

    Update ClosureUse.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- lib/PhpParser/Node/Expr/ClosureUse.php
+++ lib/PhpParser/Node/Expr/ClosureUse.php
@@ -1,3 +1,9 @@
 <?php declare(strict_types=1);
 
+namespace PhpParser\Node\Expr;
+
 require __DIR__ . '/../ClosureUse.php';
+
+if (false) {
+    class ClosureUse extends \PhpParser\Node\ClosureUse {}
+}
