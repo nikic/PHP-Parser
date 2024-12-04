00be6c99b2ccdb1bbcadbb38d4d734fc5e1abae8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-04 12:26:04 -0600

    Update StaticVar.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- lib/PhpParser/Node/Stmt/StaticVar.php
+++ lib/PhpParser/Node/Stmt/StaticVar.php
@@ -1,3 +1,9 @@
 <?php declare(strict_types=1);
 
+namespace PhpParser\Node\Stmt;
+
 require __DIR__ . '/../StaticVar.php';
+
+if (false) {
+    class StaticVar extends \PhpParser\Node\StaticVar {}
+}
