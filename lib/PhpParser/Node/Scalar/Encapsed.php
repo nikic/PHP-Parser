3fbc1ec609ca2a781c578277f43d4d953f0bbb76
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-04 12:26:04 -0600

    Update Encapsed.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- lib/PhpParser/Node/Scalar/Encapsed.php
+++ lib/PhpParser/Node/Scalar/Encapsed.php
@@ -1,3 +1,11 @@
 <?php declare(strict_types=1);
 
+namespace PhpParser\Node\Scalar;
+
+use PhpParser\Node\Scalar\InterpolatedString;
+
 require __DIR__ . '/InterpolatedString.php';
+
+if (false) {
+    class Encapsed extends InterpolatedString {}
+}
