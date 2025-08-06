605b5bd261a4530ebe70c8da8709fe7025c2fe92
Matthias Schmidt <matthias@mttsch.dev>
2025-08-06 21:19:20 +0200

    Add cast kinds for bool, int, string

--- lib/PhpParser/Node/Expr/Cast/Int_.php
+++ lib/PhpParser/Node/Expr/Cast/Int_.php
@@ -5,6 +5,10 @@ namespace PhpParser\Node\Expr\Cast;
 use PhpParser\Node\Expr\Cast;
 
 class Int_ extends Cast {
+    // For use in "kind" attribute
+    public const KIND_INT = 1; // "int" syntax
+    public const KIND_INTEGER = 2; // "integer" syntax
+
     public function getType(): string {
         return 'Expr_Cast_Int';
     }
