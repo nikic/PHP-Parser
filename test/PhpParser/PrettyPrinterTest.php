5e60691ef671a0299e1a63e9deffe08ee7ef7403
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-10-27 10:28:43 +0700

    move to test/code/formatPreservation

--- test/PhpParser/PrettyPrinterTest.php
+++ test/PhpParser/PrettyPrinterTest.php
@@ -10,7 +10,6 @@ use PhpParser\Node\InterpolatedStringPart;
 use PhpParser\Node\Scalar\Int_;
 use PhpParser\Node\Scalar\String_;
 use PhpParser\Node\Stmt;
-use PhpParser\Node\VariadicPlaceholder;
 use PhpParser\PrettyPrinter\Standard;
 
 class PrettyPrinterTest extends CodeTestAbstract {
@@ -305,36 +304,5 @@ CODE
         $this->expectException(\LogicException::class);
         $this->expectExceptionMessage('Option "indent" must either be all spaces or a single tab');
         new PrettyPrinter\Standard(['indent' => "\t  "]);
-    }
-
-    public function testTrailingCommaArgToFirstClassCallable(): void {
-        $parser = (new ParserFactory())->createForNewestSupportedVersion();
-        $traverser = new NodeTraverser(new NodeVisitor\CloningVisitor());
-
-        $oldStmts = $parser->parse(<<<'CODE'
-            <?php
-            strlen(
-                "test",
-            );
-            CODE
-        );
-        $oldTokens = $parser->getTokens();
-        $newStmts = $traverser->traverse($oldStmts);
-
-        $funcCall = $newStmts[0]->expr;
-        $funcCall->args = [new VariadicPlaceholder()];
-
-        $prettyPrinter = new PrettyPrinter\Standard();
-        $expected = <<<'CODE'
-            <?php
-            strlen(
-                ...
-            );
-            CODE;
-
-        $this->assertEquals(
-            $expected,
-            $prettyPrinter->printFormatPreserving($newStmts, $oldStmts, $oldTokens)
-        );
     }
 }
