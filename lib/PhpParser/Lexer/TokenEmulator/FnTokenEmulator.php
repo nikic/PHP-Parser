43b2ee0dba2ec0fe7c67de7505b64ab4195da092
Wouter van den Brink <git@woutervdb.com>
2026-02-26 11:54:34 +0100

    Add explanatory comment to FnTokenEmulator

--- lib/PhpParser/Lexer/TokenEmulator/FnTokenEmulator.php
+++ lib/PhpParser/Lexer/TokenEmulator/FnTokenEmulator.php
@@ -4,6 +4,7 @@ namespace PhpParser\Lexer\TokenEmulator;
 
 use PhpParser\PhpVersion;
 
+// Retained for reverse emulation support only.
 final class FnTokenEmulator extends KeywordEmulator {
     public function getPhpVersion(): PhpVersion {
         return PhpVersion::fromString('7.4');
